c2786bd537cf0d2cc0987bcd18162915ccc66697
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2024-05-11 22:22:16 -0300

    feat: update site

--- js/features.js
+++ js/features.js
@@ -3,21 +3,29 @@ const features = [
     id: 1,
     icon: 'assets/img/features/sm-spring-storage.svg',
     description: 'This is a simple storage SDK for Spring Boot applications. It provides a simple way to store and retrieve files from the file system.',
+    github: 'https://github.com/SeniorityMeter/sm-spring-storage',
+    sonatype: 'https://central.sonatype.com/artifact/br.com.senioritymeter/storage',
   },
   {
     id: 2,
     icon: 'assets/img/features/sm-spring-notification.svg',
     description: 'This is a simple notification SDK for Spring Boot applications. It provides a simple way to send notifications to users.',
+    github: 'https://github.com/SeniorityMeter/sm-spring-notification',
+    sonatype: 'https://central.sonatype.com/artifact/br.com.senioritymeter/notification',
   },
   {
     id: 3,
     icon: 'assets/img/features/sm-spring-security.svg',
     description: 'This is a simple security SDK for Spring Boot applications. It provides a simple configuration of provides authentication for your applications.',
+    github: 'https://github.com/SeniorityMeter/sm-spring-security',
+    sonatype: 'https://central.sonatype.com/artifact/br.com.senioritymeter/security',
   },
   {
     id: 4,
     icon: 'assets/img/features/sm-spring-documentation.svg',
     description: 'This is a simple documentation SDK for Spring Boot applications. It provides a simple configuration of provides documentation for your applications.',
+    github: 'https://github.com/SeniorityMeter/sm-spring-documentation',
+    sonatype: 'https://central.sonatype.com/artifact/br.com.senioritymeter/documentation',
   }
 ];
 
@@ -47,6 +55,14 @@ function createFeatureModalElement(feature) {
                   <!-- Portfolio Modal - Text-->
                   <p class="mb-4 h3">${feature.description}</p>
                 </div>
+                <div>
+                  <a href="${feature.github}" target="_blank">
+                    <img src="assets/img/link/github.png" alt="github link" width="100px" class="feature-link" />
+                  </a>
+                  <a href="${feature.sonatype}" target="_blank">
+                    <img src="assets/img/link/sonatype.jpeg" alt="github link" width="100px" class="feature-link" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -58,7 +74,7 @@ function createFeatureModalElement(feature) {
 
 function createFeatureElement(feature) {
   return `
-    <div class="col-md-6 col-lg-4 mb-5">
+    <div class="col-md-6 col-lg-3 mb-5">
       <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal${feature.id}">
         <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
           <div class="portfolio-item-caption-content text-center text-white">
